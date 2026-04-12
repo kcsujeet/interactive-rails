ae410a01a05769860392073a840c8c0f536a7bc4
Sujeet Kc <sujeetkc45@gmail.com>
2026-04-12 16:46:39 -0300

    chore: upgrade TypeScript to v6.0.2 and add @astrojs/check dependency

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+const escapeMap: Record<string, string> = {
+	'&': '&amp;',
+	'<': '&lt;',
+	'>': '&gt;',
+	'"': '&quot;',
+	"'": '&#39;',
+};
+
+export function escapeHtml(str: string): string {
+	return str.replace(/[&<>"']/g, (ch) => escapeMap[ch] ?? ch);
 }
